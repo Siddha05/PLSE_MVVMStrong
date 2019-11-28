6562e9c406b406a63df472349d9825b01c47c7ba
Assasinated <siddha05.sb@gmail.com>
2019-11-29 01:04:02 +0300

    ss1

---
+++ PLSE_MVVMStrong/ViewModel/MainVM.cs
@@ -0,0 +1,14 @@
+﻿using PLSE_MVVMStrong.Model;
+
+namespace PLSE_MVVMStrong.ViewModel
+{
+    internal class MainVM
+    {
+        #region Commands
+
+        private RelayCommand _exit;
+        public RelayCommand Exit => _exit;
+
+        #endregion Commands
+    }
+}
